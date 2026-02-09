22f47faa0544950adbe4ed4fa4ed5e08d60bb401
cucumberbatch <v_shadreeen@mail.ru>
2026-02-09 14:16:43 +0600

    Updated javadoc for EntityManager, removed App and AppTest classes

--- src/main/java/io/github/cucumberbatch/north/depot/api/EntityManager.java
+++ src/main/java/io/github/cucumberbatch/north/depot/api/EntityManager.java
@@ -1,32 +1,68 @@
 package io.github.cucumberbatch.north.depot.api;
 
-
+/**
+ * A general interface for managing entities of type {@code E} and their components.
+ *
+ */
 public interface EntityManager<E> {
 
+    /**
+     * Returns a {@link Spawn} object for performing entity instantiation.
+     *
+     * @param componentTypes is an array of component types which must be added for spawned entities
+     */
     Spawn<E> spawn(Class<?>... componentTypes);
-//    void spawnWith(SpawnFunction<E> spawnFunction, Class<?>... componentTypes);
 
+    /**
+     * Returns a {@link View} object for performing entity iteration.
+     * 
+     * @param componentTypes is an array of component types which must be present for viewed entities
+     */
     View<E> view(Class<?>... componentTypes);
-//    void forAllWith(ViewFunction<E> viewFunction, Class<?>... componentTypes);
 
-    
+    /**
+     * Attaches provided {@code component} instance to specific {@code entity}.
+     * 
+     * @param entity
+     * @param component  
+     * @return 
+     * @throws NullPointerException if {@code entity} or {@code componentType} is null
+     */
     <C> C add(E entity, C component);
 
     /**
-     * Returns a component 
+     * Returns a component of {@code componentType} that is attached to {@code entity}.
+     *
+     * @param entity
+     * @param componentType
+     * @return 
+     * @throws NullPointerException if {@code entity} or {@code componentType} is null
      */ 
     <C> C get(E entity, Class<C> componentType);
     
     /**
      * Checks if an {@code entity} has a component of provided {@code componentType}
      * 
-     * @param entity an entity reference that was previously created via
+     * @param entity an entity instance that was created via
      *   {@link org.north.core.architecture.v2.ecs.Spawn#spawn} or other similar method
      * @param componentType is a class of some object (or component)
-     * @return true if {@code entity} has a component of {@code componentType}, false - otherwise
+     * @return {@code true} if {@code entity} has a component of {@code componentType},
+     *   {@code false} - otherwise
+     * @throws NullPointerException if {@code entity} or {@code componentType} is null
      */
     <C> boolean has(E entity, Class<C> componentType);
 
+    /**
+     * Detaches component of {@code componentType} from specific {@code entity} and
+     * returns {@code true} if operation was successful, and {@code false} if such
+     * component was not found for that entity. 
+     *
+     * @param entity an entity object
+     * @param componentType a type of component that must be detached
+     * @return {@code true} if component was detached successfully, {@code false} if
+     *   such component was not found for that entity
+     * @throws NullPointerException if {@code entity} or {@code componentType} is null
+     */
     <C> boolean remove(E entity, Class<C> componentType);
 
 }
